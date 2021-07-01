7df3d7bdea65e6646f007f73f93571de48f26335
hidems <miura.eleven@gmail.com>
2021-07-02 00:23:00 +0200

    fix navigation

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,26 +1,63 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/Brazil">Brazil</router-link>
-    <router-link to="/Hawaii">Hawaii</router-link>
-    <router-link to="/Jamaica">Jamaica</router-link>
-    <router-link to="/Panama">Panama</router-link>
+    <p class="logo">The Vue School Travel App</p>
+    <ul class="nav-links">
+      <li class="links">
+        <router-link to="/">
+          Home
+        </router-link>
+      </li>
+      <li v-for="destination in destinations" :key="destination.name" class="links">
+        <router-link :to="{
+          name: 'DestinationDetails',
+          params: {id: destination.id}
+        }">
+        {{ destination.name }}
+        </router-link>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+import store from '@/store'
+export default {
+  data() {
+    return {
+      destinationId: this.$route.params.id,
+      destinations: store.destinations
+    };
+  },
+};
+</script>
+
 
 <style scoped>
 #nav {
   display: flex;
-  justify-content: center;
 }
-
 #nav a {
   font-weight: bold;
   color: #2c3e50;
-  padding: 0 10px;
+  text-decoration: none;
+  /* padding: 0 10px; */
 }
-
 #nav a.vue-school-active-class {
   color: #42b983;
+}
+.nav-links {
+  display: flex;
+}
+.links {
+  padding-right: 20px;
+  list-style: none;
+}
+.links:hover {
+  text-decoration: underline;
+}
+.logo {
+  font-size: 20px;
+  color: purple;
+  font-weight: bold;
 }
 </style>
