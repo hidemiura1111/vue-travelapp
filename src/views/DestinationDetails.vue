31f340eb30f1fefb253958980d0ee412127cc59a
hidems <miura.eleven@gmail.com>
2021-07-05 21:29:54 +0200

    Add GoBack func

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <GoBack />
     <section class="destination">
       <h1>{{ destination.name }}</h1>
       <div class="destination-details">
@@ -41,8 +42,12 @@
 
 <script>
 import store from "@/store";
+import GoBack from "@/components/GoBack";
 
 export default {
+  components: {
+    GoBack
+  },
   data() {
     return {};
   },
