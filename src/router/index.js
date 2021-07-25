2135cddeeacc6ca060a2787a477f2f49c28142c3
hidems <miura.eleven@gmail.com>
2021-07-25 19:30:19 +0200

    Add 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import store from "@/store";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
+    name: "home",
     component: Home,
     props: true,
   },
@@ -23,7 +24,23 @@ const routes = [
         props: true,
         component: () => import(/* webpackChunkName: "ExperienceDetails" */ "../views/ExperienceDetails"),
       }
-    ]
+    ],
+    beforeEnter: (to, from, next) => {
+      const exists = store.destinations.find(
+        destination => destination.slug == to.params.slug
+      );
+      if (exists) {
+        next();
+      } else {
+        next({ name: "notFound" });
+      }
+    }
+  },
+  {
+    path: "/404",
+    alias: "*",
+    name: "notFound",
+    component: () => import(/* webpackChunkName: "NotFound" */ "../views/NotFound"),
   },
 ];
 
@@ -49,5 +66,7 @@ const router = new VueRouter({
     }
   }
 });
+
+// router.beforeEach((to, from, next) => { });
 
 export default router;
